74de9a59f01f2a0885ff0a1e22dde092e926b7d4
AlekseyBabushkin <babushkin058@mail.ru>
2022-03-13 02:21:16 +0300

    added skills, portfolio. Adaptive, smooth scroll

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+const linksHeader = document.querySelectorAll('.nav__link');
+
+linksHeader.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault()
+  
+    const ID = event.target.getAttribute('href').substr(1);
+
+    document.getElementById(ID).scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+    })
+  })
+})
