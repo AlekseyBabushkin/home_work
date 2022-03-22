19c1807700bf293dd963d6deaa55095e09ddb57b
AlekseyBabushkin <babushkin058@mail.ru>
2022-03-23 00:57:26 +0300

    finish + added burger

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,9 @@
 const smoothScroll = document.querySelectorAll('.nav__link');
-const burger = document.querySelector('.burger')
-const navMenu = document.querySelector('.nav')
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
+const navItem = document.querySelectorAll('.nav__link');
+const body = document.body;
+
 
 smoothScroll.forEach((link) => {
   link.addEventListener('click', (event) => {
@@ -11,16 +14,20 @@ smoothScroll.forEach((link) => {
     document.getElementById(ID).scrollIntoView({
     behavior: 'smooth',
     block: 'start'
-    })
-  })
-})
-
-
-const toggleMenu = () => {
-  navMenu.classList.toggle('nav_active')
-  burger.classList.toggle('burger_active')
-}
+    });
+  });
+});
 
 burger.addEventListener('click', () => {
-  toggleMenu()
-})
+  body.classList.toggle('none-scroll')
+  burger.classList.toggle('burger_active')
+  nav.classList.toggle('nav_visible')
+});
+
+navItem.forEach(el => {
+  el.addEventListener('click', () => {
+    body.classList.remove('none-scroll')
+    burger.classList.remove('burger_active')
+    nav.classList.remove('nav_visible')
+  });
+});
